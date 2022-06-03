5e810c3452c3c00e4faf0fbd165bd8564f3b66da
ALDIK <93971901+ald1k0n@users.noreply.github.com>
2022-06-03 22:56:25 +0600

    Final update

--- app.js
+++ app.js
@@ -1,49 +1,272 @@
+require('dotenv').config();
 const express=require('express');
 const bodyParser=require('body-parser');
 const fs = require('fs');
 const https=require('https');
-const request=require('request');
 const lodash=require('lodash');
 const ejs=require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const { log } = require('console');
 const app=express();
+
+const secret = process.env.SECRET;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
-app.set('view engine','ejs')
+app.set('view engine','ejs');
+
+app.use(session({
+    secret: 'AnimeDB.',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+mongoose.connect("mongodb+srv://ald1k0n:test123@cluster0.ssglm.mongodb.net/AnimeDB",{useNewUrlParser:true});
+
+const accountSchema = new mongoose.Schema({
+    nick:String,
+    pass:String,
+});
+
+const commentSchema = new mongoose.Schema({
+    name:String,
+    comment:String,
+    date:String,
+    title:String
+});
+
+const favSchema = new mongoose.Schema({
+    url:String,
+    user:String
+});
+
+const newsSchema = new mongoose.Schema({
+    title:String,
+    content:String,
+    image:String
+});
+
+const Favorite = new mongoose.model("fav",favSchema);
+const News = new mongoose.model('new',newsSchema);
+const Coms = new mongoose.model("com",commentSchema);
+
+accountSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User",accountSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 //Глобальные переменные
 var commits=[];
+var news=[];
+const newsApiKey ="7a9514e3fe8441638685129d340a555b";
+const urlNews = "https://newsapi.org/v2/";
+const jikanApi = "https://api.jikan.moe/v4/top/anime?filter=airing";
+const finish = "https://api.jikan.moe/v4/top/anime?filter=bypopularity";
+const queryAPI = "https://api.jikan.moe/v4/anime?q=";
+
 
 app.get('/',function(req,res){
-    res.render('index',{commits:commits});
+    let apiUrl = urlNews + "everything?apiKey=" + newsApiKey +"&q=anime";
+    https.get(apiUrl,function(response){
+        let data = [];
+        response.on('data',function(chunk){
+            data.push(chunk);
+        });
+        response.on('end',function(){
+            var animeNews = JSON.parse(Buffer.concat(data).toString());
+            // console.log(animeNews);
+            News.find({},function(err,news){
+                if(!err){
+                    res.render('index',{news:news,animeNews:animeNews});
+                }
+            });
+            
+        });
+    });
+    
+    
+});
+
+app.get('/anonces',function(req,res){
+    https.get(jikanApi,function(response){
+        let data = [];
+        response.on('data',function(chunk){
+            data.push(chunk);
+        });
+        response.on('end',function(){
+            var angoing = JSON.parse(Buffer.concat(data).toString());
+            res.render('angoing',{angoing:angoing});
+        });
+    });
+    
 });
 
 app.get('/commit',function(req,res){
-    res.render('commit');
+    if(req.isAuthenticated() && req.session.passport.user ==="aldik"){
+        res.render('commit');
+    }
+    else{
+        res.redirect('/');
+    }
+    
+});
+
+app.get('/finished',function(req,res){
+    https.get(finish,function(response){
+        let data =[];
+        response.on('data',function(chunk){
+            data.push(chunk);
+        });
+        response.on('end',function(){
+            var angoing = JSON.parse(Buffer.concat(data).toString());
+            var sLink = "https://api.jikan.moe/v4/anime?q=";
+            var animeOBJ={
+                angoing:angoing,
+                sLink:sLink,
+                https:https,
+                lodash:lodash
+            };
+            res.render('finished',{
+                animeOBJ:animeOBJ
+            });
+        });
+    });
 });
 
 app.post('/commit',function(req,res){
-    var commitObj = {
-        animeTitle:req.body.title,
-        animeSynopsis:req.body.synopsis,
-        animePoster:req.body.image
-    };
-    commits.push(commitObj);
+    const news = new News({
+        title:req.body.newsTitle,
+        content:req.body.newsText,
+        image:req.body.image
+    });
+    news.save();
     res.redirect('/');
 });
 
-app.get('/anime/:title',function(req,res){
-    commits.forEach(function(commit){
-        let getPar=req.params.title;
-        let myPar=commit.animeTitle;
-        if(lodash.lowerCase(getPar)===lodash.lowerCase(myPar)){
-            res.render('animes',{
-                animeTitle:commit.animeTitle,
-                animePoster:commit.animePoster,
-                animeSynopsis:commit.animeSynopsis
-            });
+app.get('/news/:id',function(req,res){
+    News.findOne({_id:req.params.id},function(err,anime){
+        if(!err){
+            res.render('novosty',{
+                anime:anime
+            })
         }
     });
+});
+
+app.get('/finished/:title',function(req,res){
+    var urls= queryAPI + req.params.title;
+    https.get(urls,function(response){
+        let data = [];
+        response.on('data',function(chunk){
+            data.push(chunk);
+        });
+        response.on('end',function(){
+            let isAuth = false;
+            let nick="some";
+            var descr = JSON.parse(Buffer.concat(data).toString());
+            if(req.isAuthenticated()){
+                isAuth=true;
+                nick=req.session.passport.user;
+            }
+            Coms.find({title:req.params.title},function(err,comms){
+                if(!err){
+                    res.render('animes',{
+                        descr:descr,
+                        trailer:descr.data[0].trailer.embed_url,
+                        comms:comms,
+                        isAuth:isAuth,
+                        nick:nick
+                     });
+                }
+            })
+        });
+    });
+});
+
+app.get('/finished/title/:id',function(req,res){
+    const animeID=req.params.id;
+    const genre = "https://api.jikan.moe/v4/anime?genres="+animeID+"&order_by=start_date&sort=desc";
+    https.get(genre,function(response){
+        let data=[];
+        response.on('data',function(chunk){
+            data.push(chunk);
+        });
+        response.on('end',function(){
+            var info = JSON.parse(Buffer.concat(data).toString());
+            var sLink = "https://api.jikan.moe/v4/anime?q=";
+            var animeOBJ ={
+                angoing:info,
+                sLink:sLink,
+                
+            };
+            res.render('genres',{
+               animeOBJ:animeOBJ 
+            });
+        });
+    });
+});
+
+app.get('/finished/title/anime/:title',function(req,res){
+    const animeTitle = req.params.title;
+    const urls = queryAPI + animeTitle;
+    https.get(urls,function(response){
+        let data = [];
+        response.on('data',function(chunk){
+            data.push(chunk);
+        });
+        response.on('end',function(){
+            let isAuth = false;
+            let nick="some";
+            var descr = JSON.parse(Buffer.concat(data).toString());
+            if(req.isAuthenticated()){
+                isAuth=true;
+                nick=req.session.passport.user;
+            }
+            Coms.find({title:req.params.title},function(err,comms){
+                if(!err){
+                    res.render('animes',{
+                        descr:descr,
+                        trailer:descr.data[0].trailer.embed_url,
+                        comms:comms,
+                        isAuth:isAuth,
+                        nick:nick
+                     });
+                }
+            })
+            
+            
+        });
+    });
+});
+
+app.post('/search',function(req,res){
+    const searchLine  = req.body.search;
+    const searchQuery = queryAPI + searchLine;
+    https.get(searchQuery,function(response){
+        let data=[];
+        response.on('data',function(chunk){
+            data.push(chunk);
+        });
+        response.on('end',function(){
+            var searched = JSON.parse(Buffer.concat(data).toString());
+            res.render('search',{
+                animeTitle:searched
+            });
+        });
+    });
+    
 });
 
 app.get('/popular',function(req,res){
@@ -56,14 +279,6 @@ app.get('/bunnyGirl.html',function(req,res){
 
 app.get('/finished',function(req,res){
     res.sendFile(__dirname + '/finished.html');
-});
-
-app.get('/anonces',function(req,res){
-    res.send("Анонсы на сегодня");
-});
-
-app.get('/profile',function(req,res){
-    res.send("Авторизация");
 });
 
 app.get('/opm.html',function(req,res){
@@ -102,54 +317,168 @@ app.get('/bezRab.html',function(req,res){
     res.sendFile(__dirname+'/bezRab.html');
 });
 
-//API key for mailchimp e414c07b31d1df3c3182e3e7728d48d1-us14 
-//Unique id for list id : a2aecfd4e2
+app.get('/profile',function(req,res){
+    if(!req.isAuthenticated()){
+        res.render('profile',{nick:true});
+    }else{
+        Favorite.find({user:req.session.passport.user},function(err,item){
+            if(!err){
+                // console.log(item.length);
+                res.render('account',{name:req.session.passport.user,item:item});
+            }
+        });
+    }
+});
 
-app.get('/register.html',function(req,res){
-    res.sendFile(__dirname+'/register.html');
+app.post('/profile',function(req,res){
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+       });
+    
+       req.login(user,function(err){
+           if(err){
+               console.log(err);
+           }
+           else{
+               passport.authenticate('local')(req,res,function(err){
+                   if(!err){
+                    res.redirect('/profile');
+                   }
+                   else{
+                       res.render('login',{error:"Login or password isn't defined"})
+                   }
+               });
+           }
+       })
+});
+
+app.post('/logout',function(req,res){
+    req.logout();
+    res.redirect('/');
+});
+
+app.get('/account',function(req,res){
+    if(req.isAuthenticated()){
+        res.render('account',{name:req.session.passport.user});
+    }
+    else{
+        res.redirect('/profile');
+    }
+});
+
+app.get('/register',function(req,res){
+    res.render('register');
+});
+
+app.get('/admin',function(req,res){
+    if(req.isAuthenticated() && req.session.passport.user ==="aldik"){
+        News.find({},function(err,news){
+            if(!err){
+                res.render('admin',{
+                    news:news
+                });
+            }
+        });
+        
+    }
+    else{
+        res.redirect('/');
+    }
 });
 
 app.post('/register',function(req,res){
-    var nick=req.body.nick;
-    var email=req.body.email;
-    var pass=req.body.pass;
-
-    var data={
-        members:[
-            {
-                email_address:email,
-                status:"subscribed",
-                merge_fields:{
-                    NNAME:nick,
-                    PASS:pass
-                }
-            }
-        ]
-    }
-
-    const jsonData=JSON.stringify(data);
-    const url="https://us14.api.mailchimp.com/3.0/lists/a2aecfd4e2";
-    const options = {
-        method: "POST",
-        auth: "ALD1K:e414c07b31d1df3c3182e3e7728d48d1-us14"
-    }
-
-    const request = https.request(url,options, function(response){
-        if(response.statusCode===200){
-            res.send("<h1>Success</h1>");
+    User.register({username:req.body.username}, req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect('/register');
         }
         else{
-            res.send("<h1>Failure</h1>");
+            passport.authenticate("local")(req,res,function(){
+                res.redirect('/profile');
+            });
         }
-        response.on('data',function(data){
-            console.log(JSON.parse(data));
-        });
     });
-        request.write(jsonData);
-        request.end();
+});
+
+app.post('/delNews',function(req,res){
+    News.findByIdAndRemove(req.body.del,function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.redirect('/admin');
+        }
+    })
+});
+
+app.post('/finished/:coms',function(req,res){
+    const date = new Date().toLocaleString();
+
+    const comment = new Coms({
+        name:req.session.passport.user,
+        comment:req.body.comment,
+        date:date,
+        title:req.params.coms
+    });
+
+    comment.save();
+    res.redirect('/finished/' + req.params.coms);
+});
+
+app.post('/remove/:name',function(req,res){
+    Coms.findByIdAndRemove(req.body.delBtn,function(){
+        res.redirect('/finished/' + req.params.name );
+    });
+
+});
+
+app.post('/fav',function(req,res){
+    if(req.isAuthenticated()){
+        console.log(req.body.fav);
+        const fav = new Favorite({
+            url:"/finished/" + req.body.fav,
+            user:req.session.passport.user
+        });
+        fav.save();
+        res.redirect('/finished/' + req.body.fav);
+    }
+    else{
+        res.redirect('/profile');
+    }
+
 });
 
 
-app.listen(3000,function(){
+//dfdg
+app.get('/sdf',function(req,res){
+    res.send("help");
+    const options = {
+        "method": "GET",
+        "hostname": "animenewsnetwork.p.rapidapi.com",
+        "port": null,
+        "path": "/reports.xml?id=155&nskip=50&nlist=50",
+        "headers": {
+            "X-RapidAPI-Host": "animenewsnetwork.p.rapidapi.com",
+            "X-RapidAPI-Key": "531b747faemshe6812b12ca07561p176de1jsn412041e5f57c",
+            "useQueryString": true
+        }
+    };
+    const requ = https.request(options, function (res) {
+        const chunks = [];
+    
+        res.on("data", function (chunk) {
+            chunks.push(chunk);
+        });
+    
+        res.on("end", function () {
+            const d = Buffer.concat(chunks);
+            console.log(d);
+        });
+    });
+});
+
+app.listen(process.env.PORT || 3000,function(){
     console.log('localhost:3000');
 });
